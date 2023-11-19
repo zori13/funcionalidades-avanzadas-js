4014fdd9f170d021415f1bd16c2ef96ea71e717d
zori13 <zori.gon@mail.com>
2023-11-19 21:58:56 +0100

    Ejercicio 10

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -1,1 +1,13 @@
-// Escribe tu código aquí:
+const personas= new Map()
+personas.set('Amparo', 55)
+personas.set('Javier', 45)
+personas.set('Josito', 35)
+personas.set('Lara', 25)
+
+console.log('Edad de Amparo:', personas.get('Amparo'))
+console.log(personas)
+
+const borrarPersona='Javier'
+personas.delete(borrarPersona)
+console.log('personas que quedan en la lista:',personas)
+
