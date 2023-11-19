f55d13f5b90e3d62f32fbdb20b7a25bd1d4f5966
zori13 <zori.gon@mail.com>
2023-11-19 21:21:08 +0100

    Ejercicio 5

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,9 @@
-// Escribe tu código aquí:
+const comida={
+    nombre: 'alubias',
+    precio: 1.59,
+    categoria: 'legumbre'
+}
+const {nombre,precio,categoria}=comida
+console.log(`Nombre: ${nombre}`)
+console.log(`Precio: ${precio}`)
+console.log(`Categoría: ${categoria}`)
