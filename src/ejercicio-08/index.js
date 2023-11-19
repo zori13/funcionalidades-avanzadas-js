9ce933c7f0b83f4ac878965f5bef02e3035e3ae3
zori13 <zori.gon@mail.com>
2023-11-19 21:39:40 +0100

    Ejercicio 8

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,5 @@
-// Escribe tu código aquí:
+function suma(...num){
+    return num.reduce((total,num)=> total +num,0)
+}
+const resultado=suma(45,22,57,85,11)
+console.log(`La suma es: ${resultado}`)
